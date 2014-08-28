09d2271bc23716dc5ddfb3719924c017b1a6f53d
shota <s.sugimoto.419@gmail.com>
2014-08-29 01:30:09 +0900

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+ resources :users
   get 'users/new'
 
   root :to =>  'static_pages#home'
   get 'static_pages/home'
+
  match '/signup',  to: 'users#new',            via: 'get'
   match '/help',    to:   'static_pages#help',    via: 'get'
   match '/about',   to:  'static_pages#about',   via: 'get'
